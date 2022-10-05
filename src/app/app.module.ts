56c8044f52e24a86fa0e85a8d77e28e8820c34db
FrancoL <francovalentin5005@gmail.com>
2022-10-05 20:52:59 -0300

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './singUp/singUp.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent],
+	declarations: [AppComponent, SingUpComponent, LoginComponent],
 	imports: [BrowserModule, MatButtonModule, FormsModule, ReactiveFormsModule,MatCardModule, HttpClientModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/singUp/singUp.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UsuarioService } from '../usuarios.service';
+import {Subscription} from "rxjs"
+
+
+@Component({
+	selector: 'app-singUp',
+	templateUrl: './singUp.component.html',
+	styleUrls: ['./singUp.component.css'],
+})
+export class SingUpComponent implements OnDestroy {
+  constructor(private fb: FormBuilder, private Usuario: UsuarioService) {}
+  public validPassword: boolean = true;
+  public userSubscription!:Subscription;
+
+	form: FormGroup = this.fb.group({
+		nombre: [, [Validators.required]],
+		apellido: [, [Validators.required]],
+		nombreUsuario: [, [Validators.required]],
+		contrasena: [, [Validators.required]],
+		confirmarContrasena: [, [Validators.required]],
+		pais: [, [Validators.required]],
+		ciudad: [, [Validators.required]],
+	});
+	enviarDatos() {
+		// user_name: userName,
+		//     name,
+		//     last_name: lastName,
+		//     country,
+		//     city,
+		//     password,
+    
+    if(this.form.controls['contrasena'].value !== this.form.controls['confirmarContrasena'].value){
+      this.validPassword = false
+      return
+    }
+    this.validPassword = true
+    
+		if (this.form.invalid ) {
+			this.form.markAllAsTouched();
+			return;
+		}
+		const {
+			nombre: name,
+			ciudad: city,
+			nombreUsuario: user_name,
+			contrasena: password,
+			pais: country,
+			apellido: last_name,
+		} = this.form.value;
+    
+
+		this.userSubscription = this.Usuario.createUser({
+			name,
+			city,
+			user_name,
+			password,
+			country,
+			last_name,
+		}).subscribe(data => console.log(data))
+	}
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe()
+  }
+}
